f20789147a44654b3c8b853c7e9f08918b5a5843
Onurakdgn <86933632+Onurakdgn@users.noreply.github.com>
2022-09-24 17:53:53 +0300

    code

---
+++ KodlamaIOJava2022-Odev2/classes/src/classes/CustomerManager.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class CustomerManager {
+	// operasyonlarımızı burada yapaçağız.
+	public void Add() {
+		System.out.println("Müşteri eklendi.");
+	}
+
+	public void remove() {
+		System.out.println("Müşteri silindi");
+	}
+
+	public void update() {
+		System.out.println("Müşteri güncellendi.");
+	}
+
+}
